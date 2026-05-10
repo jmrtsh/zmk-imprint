8f5e4f3fd9dd0a3b1c7dfb31609481f35f561355
Jim M. R. Teichgräber <a@teichgraeber.digital>
2026-05-10 12:52:43 +0200

    [NFC] comments

--- config/imprintKeyPositionIndices.h
+++ config/imprintKeyPositionIndices.h
@@ -1,3 +1,5 @@
+// TODO this is slightly wrong, for the thumbs at least, I'm pretty sure
+
 /*                          CYBOARD IMPRINT KEY MATRIX / LAYOUT MAPPING
  ╭────────────────────────────────────╮       ╭────────────────────────────────────╮
  │  0   1   2   3   4   5             │       │              6   7   8   9  10  11 │
